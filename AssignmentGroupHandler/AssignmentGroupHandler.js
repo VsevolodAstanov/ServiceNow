0eda5129db3f8391140054e5f9834a5f35a323b8
Vsevolod Astanov <vsevolod.astanov@gmail.com>
2018-11-20 23:14:58 +0300

    Assgnment Group v0.02: Parse data

--- AssignmentGroupHandler/AssignmentGroupHandler.js
+++ AssignmentGroupHandler/AssignmentGroupHandler.js
@@ -15,8 +15,10 @@ function AssignmentGroupsHandler() {
 
 AssignmentGroupsHandler.prototype.createGroups = function() {
 
-	this.parser.setSheetName("Assignment Groups"); // XSLX Tab name
-	this.parser.parse(this.attStr);
+	var _this = this;
+
+	_this.parser.setSheetName("Assignment Groups"); // XSLX Tab name
+	_this.parser.parse(_this.attStr);
 
 	/*
 		Available data to transform from XSLX to ServiceNow Fields/Related Lists:
@@ -33,8 +35,8 @@ AssignmentGroupsHandler.prototype.createGroups = function() {
 
 	var groups = [];
 
-	while(this.parser.next()) { 
-		var r = this.parser.getRow(); 
+	while(_this.parser.next()) { 
+		var r = _this.parser.getRow(); 
 		groups.push({
 			name: r["Assignment group name"],
 			manager: r["Group Manager"],
@@ -42,15 +44,62 @@ AssignmentGroupsHandler.prototype.createGroups = function() {
 			description: r["Description"],
 			location: r["Location"],
 			types: r["Group type"].replace(/\s+/g,"").split(","),
-			members: r["Group Members"].split(","),
-			roles: r["Roles"].replace(/\s+/g,"").split(",")		
+			members: r["Group Members"],
+			roles: r["Roles"].replace(/\s+/g,"").split(",")
 		});
 	}
 
 	if(groups.length == 0)
 		return gs.info("Group list is empty");
 
+	var users = _this.getUsers(groups);
+	var types = _this.getTypes(groups);
+	var roles = _this.getRoles(groups);
+
+	groups.forEach(function(group) {
+
+		var groupGR = new GlideRecord('sys_user_group');
+		groupGR.initialize();
+		groupGR.name = group.name;
+		groupGR.manager = users[group.name];
+		groupGR.company = group.company;
+		groupGR.description = group.description;
+		groupGR.location = group.location;
+		groupGR.types = group.types;
+
+	});
+
 	gs.info(groups[0].members[0]);
+}
+
+AssignmentGroupsHandler.prototype.getUsers = function(groups) {
+
+	var notVerifiedUsers = "";
+
+	groups.forEach(function(group) {
+		notVerifiedUsers += group.manager + "," + group.members;
+	});
+
+	var users = {};
+	var usersGR = new GlideRecord("sys_user");
+	usersGR.addActiveQuery();
+	usersGR.addQuery("name", "IN", notVerifiedUsers);
+	usersGR.addNotNullQuery("email");
+	usersGR.query();
+
+	while(usersGR.next()) {
+		users[usersGR.name.getDisplayValue()] = usersGR.getUniqueValue();
+	}
+
+	return users;
+}
+
+AssignmentGroupsHandler.prototype.getTypes = function(groups) {
+
+}
+
+AssignmentGroupsHandler.prototype.getRoles = function(groups) {
+
 }
 
 new AssignmentGroupsHandler().createGroups();
