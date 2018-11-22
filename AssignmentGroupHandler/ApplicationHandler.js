6d49a21b05e72aa5a628fb0c50aa5ee0fa030930
Vsevolod Astanov <Vsevolod_Astanov@epam.com>
2018-11-22 17:01:31 +0300

    Application CI: V0.2

--- AssignmentGroupHandler/ApplicationHandler.js
+++ AssignmentGroupHandler/ApplicationHandler.js
@@ -50,6 +50,8 @@ function ApplicationHandler() {
 			});
 		}
 
+		gs.info(applications[0].name)
+
 		if(applications.length == 0) {
 			gs.info("Application list is empty");
 			return;
@@ -65,8 +67,7 @@ function ApplicationHandler() {
 			"sys_user",
 			"cmn_location",
 			"sys_user_group",
-			"u_org_hierarchy",
-			"core_company"
+			"u_org_hierarchy"
 		];
 
 		for(ql = 0; ql < queryTables.length; ql++) {
@@ -74,8 +75,66 @@ function ApplicationHandler() {
 			queryStore[queryTables[ql]] = "";
 		}
 
+		applications.forEach(function(appl) {
+			queryStore.sys_user += appl.owned_by + "," + appl.managed_by + ",";
+			queryStore.cmn_location += appl.location + ",";
+			queryStore.sys_user_group += appl.support_group + "," + appl.change_control + ",";
+			queryStore.u_org_hierarchy += appl.u_heir2 + ",";
+		});
 
-	}
+		gs.info(queryStore.cmn_location);
+
+		//Remove Duplicates
+		for (var q in queryStore) {
+			queryStore[q] = queryStore[q].split(",").filter(function(value, index, self) { 
+			    return self.indexOf(value) === index;
+			}).filter(Boolean).join(',');
+		}
+
+		var glideQueryHandler = function(table, q) {
+			var gr = new GlideRecord(table);
+			gr.addActiveQuery();
+			if(table == "u_org_hierarchy") {
+				gr.addQuery("u_name", "IN", q);
+			} else {
+				gr.addQuery("name", "IN", q);
+			}
+			if(table == "sys_user"){
+				gr.addNotNullQuery("email");
+				gr.addEncodedQuery("emailNOT LIKEa-")
+			}
+			gr.query();
+
+			while(gr.next()) {
+
+				var nameKey = (gr.name ? gr.name.getDisplayValue() : gr.u_name.getDisplayValue());
+
+				_this[table][nameKey] = gr.getUniqueValue();
+				var arrQuery = queryStore[table].split(",");
+				gs.info(arrQuery);
+				arrQuery.splice(arrQuery.indexOf(nameKey), 1);
+				queryStore[table] = arrQuery.join(",");
+				gs.info(arrQuery);
+			}
+		}
+
+		for(var q in queryStore) {
+			glideQueryHandler(q, queryStore[q])
+		}
+
+		var failQuery;
+		for (var qu in queryStore) {
+			gs.info(typeof queryStore[qu]);
+			if(queryStore[qu].split(",").length > 0) {
+				gs.info("Wrong values on table : " + qu);
+				gs.info(queryStore[qu].length);
+				gs.info(queryStore[qu]);
+				failQuery = true;
+			}
+		}
+
+		return !failQuery;
+	};
 
 	this.removeSpaces = function(str) {
 		return str.replace(/\s+(?!,\s,)\s+|(?!\s+),\s+|,/g,",");
