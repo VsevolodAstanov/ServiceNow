707f679672c5e9828ef2eec61b7760433c95c94c
Vsevolod Astanov <vsevolod.astanov@gmail.com>
2019-02-04 21:38:35 +0300

    Data Mapping: Update Groups/BS/TS/Apps v1.0

--- DataMapping/DataMapping.js
+++ DataMapping/DataMapping.js
@@ -9,7 +9,7 @@ function DataMapping(type) {
 	ga.query();
 
 	if (!ga.next()) {
-		gs.info("Attchment is not defined");
+		gs.log("Attchment is not defined", "Incident Data Mapping");
 		return;
 	}
 
@@ -17,7 +17,7 @@ function DataMapping(type) {
 
 	this.map = function() {
 
-		gs.info("Start mapping for: " + type);
+		gs.log("Start mapping for: " + type, "Incident Data Mapping");
 
 		try {
 			if (!type)
@@ -40,23 +40,29 @@ function DataMapping(type) {
 			}
 
 		} catch (err) {
-			gs.info("Error: " + err);
+			gs.log("Error: " + err, "Incident Data Mapping");
 		}
 	};
 
 	this._groupToGroup = function(data) {
+		gs.log(JSUtil.describeObject(data), "Incident Data Mapping");
 		self._replaceToSysIDs(["sys_user_group"], data);
+		gs.log(JSUtil.describeObject(data), "Incident Data Mapping");
 		self._updateMappedRecord(["incident", "sc_task"], data);
 	};
 
 	this._tsToTS = function(data) {
+		gs.log(JSUtil.describeObject(data), "Incident Data Mapping");
 		self._replaceToSysIDs(["u_cmdb_ci_technical_service"], data);
+		gs.log(JSUtil.describeObject(data), "Incident Data Mapping");
 		self._updateMappedRecord(["incident"], data);
 	};
 
 	this._bsToBS = function(data) {
-		self._replaceToSysIDs(["cmdb_ci_service", "cmdb_ci_appl"], data);
-		//self._updateMappedRecord(["incident"], data);
+		gs.log(JSUtil.describeObject(data), "Incident Data Mapping");
+		self._replaceToSysIDs(["cmdb_ci"], data);
+		gs.log(JSUtil.describeObject(data), "Incident Data Mapping");
+		self._updateMappedRecord(["incident"], data);
 	};
 
 	this._parseData = function() {
@@ -87,76 +93,97 @@ function DataMapping(type) {
 				o["new"] = new_key;
 
 				if(app)
-					o['cmdb_ci_appl'] = app;
+					o['app'] = app;
 
 				data[type].push(o);
 			}
 
 			return data;
 		} catch(e) {
-			gs.info("Error: " + e);
+			gs.log("Error: " + e, "Incident Data Mapping");
 		}
 	};
 
-	this._replaceToSysIDs = function(tables, data) {
-		tables.forEach(function(table) {
+	this._replaceToSysIDs = function(table, data) {
 
-			var gr = new GlideRecord(table);
-			var query = ""
+		var gr = new GlideRecord(table);
+		var query = "";
+		switch(type) {
+			case "Groups":
+				var groups = data[type];
+				for(var g = 0; g < groups.length; g++) {
+					query += "^ORname=" + groups[g]["old"] + "^ORname=" + groups[g]["new"];
+				}
+				break;
+
+			case "Technical Services":
+				var tech_serv = data[type];
+				for(var ts = 0; ts < tech_serv.length; ts++) {
+					query += "^ORname=" + tech_serv[ts]["old"] + "^ORname=" + tech_serv[ts]["new"];
+				}
+				break;
+
+			case "Business Services":
+				var bs_app = data[type];
+				for(var ba = 0; ba < bs_app.length; ba++) {
+					query += "^NQname=" + bs_app[ba]["old"] + "^sys_class_name=cmdb_ci_service^operational_status=2";
+					query +=  "^NQname=" + bs_app[ba]["new"] + "^sys_class_name=cmdb_ci_service^location!=null^parent!=null";
+					query +=  "^NQname=" + bs_app[ba]["new"] + "^sys_class_name=cmdb_ci_service^parent=null";
+					if(bs_app[ba]["app"])
+						query += "^NQname=" + bs_app[ba]["app"] + "^sys_class_name=cmdb_ci_appl";
+				}
+				break;
+		}
+
+		gr.addEncodedQuery(query);
+		gr.query();
+		gs.log("Rercords matched: " + gr.getRowCount() + "\n", "Incident Data Mapping");
+		while(gr.next()) {
+
+			var name = gr.name.getDisplayValue();
+			var id = gr.getUniqueValue();
+
 			switch(type) {
 				case "Groups":
-					var groups = data[type];
+
 					for(var g = 0; g < groups.length; g++) {
-						query += "^ORname=" + groups[g]["old"] + "^ORname=" + groups[g]["new"];
+						if(groups[g]["old"] == name)
+							groups[g]["old"] = id;
+
+						if(groups[g]["new"] == name)
+							groups[g]["new"] = id;
 					}
+					
 					break;
 
 				case "Technical Services":
-					var tech_serv = data[type];
+
 					for(var ts = 0; ts < tech_serv.length; ts++) {
-						query += "^ORname=" + tech_serv[ts]["old"] + "^ORname=" + tech_serv[ts]["new"];
+						if(tech_serv[ts]["old"] == name)
+							tech_serv[ts]["old"] = id;
+
+						if(tech_serv[ts]["new"] == name)
+							tech_serv[ts]["new"] = id;
 					}
+					
 					break;
 
 				case "Business Services":
-					var bus_serv = data[type];
-					for(var ts = 0; ts < bus_serv.length; ts++) {
-						query += "^ORname=" + bus_serv[ts]["old"] + "^ORname=" + bus_serv[ts]["new"];
+
+					for(var ba = 0; ba < bs_app.length; ba++) {
+						if(bs_app[ba]["old"] == name && gr.getValue('operational_status') == 2)
+							bs_app[ba]["old"] = id;
+
+						if(bs_app[ba]["new"] == name && gr.getValue('operational_status') == 1)
+							bs_app[ba]["new"] = id;
+
+						if(bs_app[ba]["app"] == name)
+							bs_app[ba]["app"] = id;
 					}
+					
 					break;
 			}
-
-			gr.addEncodedQuery(query);
-			gr.query();
-			gs.info("Rercords matched: " + gr.getRowCount() + "\n");
-			while(gr.next()) {
-
-				var name = gr.name.getDisplayValue();
-				var id = gr.getUniqueValue();
-
-				switch(type) {
-					case "Groups":
-
-						for(var g = 0; g < groups.length; g++) {
-							if(groups[g]["old"] == name)
-								groups[g]["old"] = id;
-
-							if(groups[g]["new"] == name)
-								groups[g]["new"] = id;
-						}
-
-					case "Technical Services":
-
-						for(var ts = 0; ts < tech_serv.length; ts++) {
-							if(tech_serv[ts]["old"] == name)
-								tech_serv[ts]["old"] = id;
-
-							if(tech_serv[ts]["new"] == name)
-								tech_serv[ts]["new"] = id;
-						}
-				}
-			} 
-		});
+		}
 	};
 
 	this._updateMappedRecord = function(tables, data) {
@@ -167,7 +194,7 @@ function DataMapping(type) {
 			gr.addActiveQuery();
 			gr.addQuery('sys_created_on', ">=", "javascript:gs.dateGenerate('2018-01-01','00:00:00')");
 
-			gs.info("Type: " + type);
+			gs.log("Type: " + type, "Incident Data Mapping");
 			switch(type) {
 				case "Groups":
 
@@ -187,14 +214,15 @@ function DataMapping(type) {
 					}
 					break;
 
-				// case "Business Services":
-				// 	gr.addQuery("u_business_service", 'IN', Object.keys(data).join(','));
-				// 	break;
+				case "Business Services":
+					var bs_app = data[type];
+
+					for(var ba = 0; ba < bs_app.length; ba++) {
+						query += "^ORu_business_service=" + bs_app[ba]["old"];
+					}
+					break;
 			}
 
-			//query += "^sys_created_on>=javascript:gs.dateGenerate('2018-01-01','00:00:00')"
-
-			//gs.info(query);
 			gr.addEncodedQuery(query);
 			gr.orderBy('number');
 			gr.query();
@@ -207,11 +235,15 @@ function DataMapping(type) {
 							if(gr.getValue('assignment_group') == groups[g]["old"]) {
 								gr.setValue('assignment_group', groups[g]["new"]);
 
-								//gs.info(groups[g]["old"] + " : " + groups[g]["new"]);
+								gs.log(groups[g]["old"] + " : " + groups[g]["new"], "Incident Data Mapping");
+
+								self._update(gr);
 
 								continue recordCycle;
 							}
 						}
+						
+						break;
 
 					case "Technical Services":
 						var tech_serv = data[type];
@@ -220,23 +252,47 @@ function DataMapping(type) {
 							if(gr.getValue('u_technical_service') == tech_serv[ts]["old"]) {
 								gr.setValue('u_technical_service', tech_serv[ts]["new"]);
 
-								//gs.info(tech_serv[g]["old"] + " : " + tech_serv[g]["new"]);
+								self._update(gr);
 
 								continue recordCycle;
 							}
 						}
+						
+						break;
+
+					case "Business Services":
+						var bs_app = data[type];
+
+						for(var ba = 0; ba < bs_app.length; ba++) {
+							if(gr.getValue('u_business_service') == bs_app[ba]["old"]) {
+								gr.setValue('u_business_service', bs_app[ba]["new"]);
+
+								if(gr.getValue('u_application_ci') == bs_app[ba]["app"])
+									gr.setValue('u_application_ci', bs_app[ba]["app"])
+
+								self._update(gr);
+
+								continue recordCycle;
+							}
+
+							
+						}
+							
+						break;
 				}
-				//gr.setWorkflow(false);
-				//gr.update();
-				
-				//gs.info(gr.assignment_group.getDisplayValue());
 			}
 
-			gs.print("\n\ttTable: " + table + "\n\tRecord: " + gr.getRowCount());
+			gs.log("\n\tTable: " + table + "\n\tRecord: " + gr.getRowCount(), "Incident Data Mapping");
 		});
+	};
+
+	this._update = function(gr) {
+		gr.autoSysFields(false);
+		gr.setWorkflow(false);
+		gr.update();
 	};
 }
 
-//new DataMapping("Groups").map();
+new DataMapping("Groups").map();
 new DataMapping("Business Services").map();
-//new DataMapping("Technical Services").map();
+new DataMapping("Technical Services").map();
